b1554e319935f85862332c42e615ac602294e41a
randytsao24 <randytsao24@gmail.com>
2023-02-09 14:38:01 -0500

    Add Tailwind to packages and set up header

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='p-6 max-w-sm mx-auto flex items-center space-x-4'>
+      <h1 className="text-3xl font-bold underline">
+        suppppppp
+      </h1>
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className='bg-black w-screen text-white shadow-lg border-4 border-t-blue-300 border-r-red-300 border-b-green-300 border-l-yellow-300'>
+      <div className='p-5 font-mono text-xl'>
+        <span className='underline'>
+          Randy Tsao
+        </span>
+        <span className='animate-pulse text-green-300 text-2xl'>
+          .
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
