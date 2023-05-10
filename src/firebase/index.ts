e58dcced9345e04feafa6e0d8ad87118d7f495ad
randytsao24 <randytsao24@gmail.com>
2023-05-10 18:06:26 -0400

    Add note about secrets

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,6 +6,8 @@ import admin from 'firebase-admin';
 // Initialize Firebase
 let app;
 
+// NOTE: Apparently exposing these values is okay, according to:
+// https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different
 const config = {
   apiKey: 'AIzaSyCEDpTymusBauoW6yUxWg6tcZm_EzKRjx0',
   appId: "1:42039643605:web:1e000eed41b150742cc15c",
